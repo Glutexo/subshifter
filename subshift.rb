af1109ccb6f0d71746d6a6c2f575c95a89a94628
Štěpán Tomsa <stepantomsa@icloud.com>
2017-08-14 19:01:46 +0200

    Read input from ARGF.
    Fixed input argument changed to ARGF. Duration moved to the first
    position so a list of files can follow. Ability to read from STDIN
    automatically gained.

--- subshift.rb
+++ subshift.rb
@@ -3,18 +3,21 @@
 $:.unshift "#{File.dirname(__FILE__)}/lib"
 require "subtitle"
 
-if ARGV[0].nil? or ARGV[1].nil?
-  puts 'Usage: subshift.rb <file> <duration>'
-  puts '  <duration> is in seconds.'
-  puts '    Positive number – subtitles will appear later.'
-  puts '    Negative number – subtitles will appear sooner.'
-  puts
-  puts 'Example: subshift.rb Avatar.srt -10'
+begin
+  shift = Float(ARGV.shift)
+rescue ArgumentError, TypeError
+  puts <<-HELP
+Usage: subshift.rb <duration> [files]
+  <duration> is in seconds and can be decimal.
+    Positive number – subtitles will appear later.
+    Negative number – subtitles will appear sooner.
+
+Example: ruby subshift.rb -10.7 subtitle.srt
+  HELP
   exit
 end
 
-source = File.new ARGV[0]
-subtitle = Subtitle.new source
-subtitle.shift! ARGV[1].to_f
+subtitle = Subtitle.new $<.read
+subtitle.shift! shift
 
 File.write 'shifted.srt', subtitle
